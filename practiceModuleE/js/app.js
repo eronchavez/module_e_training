7d3ce4570b0fca1cde85f638fbe47ce993acd1b3
Aaron <aaronchavez.dev@gmail.com>
2026-05-04 15:44:46 +0800

    added video function js: playground

--- practiceModuleE/js/app.js
+++ practiceModuleE/js/app.js
@@ -1,4 +1,5 @@
-// Map Markers
+// map marker
+
 let markers = document.querySelectorAll(".marker");
 let attractionCard = document.querySelectorAll(".attractionCard");
 
@@ -9,6 +10,47 @@ markers.forEach((marker,index) => {
     marker.addEventListener("mouseout", () => {
         attractionCard[index].classList.remove("focus");
     });
-
-
 });
+
+
+
+// Read loud 
+document.getElementById("readLoud").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const speech = new SpeechSynthesisUtterance("Contact: 0992903087");
+    speech.lang = "fr-FR";
+    window.speechSynthesis.speak(speech);
+})
+
+
+// Video 
+
+const video = document.querySelector('video');
+
+if(video)
+{
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting)
+            {
+                entry.target.play();
+            }else{
+                entry.target.pause();
+            }
+        });
+    },{
+        threshold: 0.5
+    });
+
+    observer.observe(video);
+
+    document.addEventListener("visibilitychange", ()=> {
+        if(document.visibilityState === 'hidden')
+        {
+            video.pause();
+        }else{
+            video.play();
+        }
+    });
+}
