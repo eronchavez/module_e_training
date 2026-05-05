7e0c0a05913c9520a1790d2754ee8221b660bb0b
Aaron <aaronchavez.dev@gmail.com>
2026-05-05 12:00:01 +0800

    Practicing Call To Action

--- practiceModuleE/js/app.js
+++ practiceModuleE/js/app.js
@@ -55,40 +55,69 @@ if(video)
     });
 }
 
-// Coupon Game Event
-function couponGame()
+
+// Coupon Event 
+
+function couponEvent()
 {
-    //Target the elements from HTML
     const result = document.getElementById("result");
-    const draw = document.getElementById("drawBtn");
+    const drawBtn = document.getElementById("drawBtn");
     const restart = document.getElementById("restart");
-    //When user click the draw btn
-    draw.onclick = () => {
+
+    drawBtn.onclick = () => 
+    {
         const rand = Math.floor(Math.random() * 3) + 1;
         const prizes = {
-            1: "10% Discount Coupon!",
-            2: "No Prize!",
-            3:"100% Discount Coupon"
+            1: "10% discount Coupon",
+            2: "No Coupon",
+            3: "100% Coupon Discount!"
         };
 
-            
         result.textContent = prizes[rand];
         result.className = "active";
 
-        draw.disabled = true;
+        drawBtn.disabled = true;
         restart.disabled = false;
         restart.classList.add("highlight");
-    };
+        drawBtn.classList.add("highlight");
+    }
+
 
     restart.onclick = () => {
         result.textContent = "";
         result.className = "";
-        
-        draw.disabled= false;
-        restart.disabled= true;
+
+        drawBtn.disabled = false;
+        restart.disbled = true;
+        restart.classList.remove("highlight");
         restart.classList.remove("highlight");
     };
 
 }
 
-couponGame();
+
+// let cto = document.querySelector("#cto");
+// let mask = document.querySelector("#cto #mask");
+
+// cto.addEventListener("mousemove", handleMouseMove);
+
+// function handleMouseMove(e){
+//     const rec = this.getBoundingClientRect();
+//     const mouse_x = e.clientX-rec.left-rec.width / 2;
+//     const mouse_y = e.clientY-rec.top-rec.height / 2;
+//     let angle = Math.atan2(mouse_y,mouse_x) * (180 / Math.PI);
+//     angle = (angle + 360) % 360;
+
+//     this.style.setProperty("--start", angle + 60);
+
+//     const x = parseInt((e.clientX - rec.left) / rec.width * 100);
+//     const y = parseInt((e.clientY - rec.top) / rec.height * 100);
+//     console.log(x);
+//     console.log(y);
+//     mask.style.setProperty("--mouse-x", x + "%");
+//     mask.style.setProperty("--mouse-y", y + "%");
+
+// }
+
+
+couponEvent();
